1754b870b9c65c288cdb258b424c08948c0446fd
Vguetz <gomezlucas712@gmail.com>
2023-07-17 17:23:41 -0300

    baby-steps

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	background: {
+		'border-radius': '0.8rem',
+		'border': '1px solid #1E2D3D',
+		'background': '#011627',
+	},
+}
