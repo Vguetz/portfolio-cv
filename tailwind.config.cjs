5626ad4a20da1d543d7136e5c44e10add09aba7d
Vguetz <gomezlucas712@gmail.com>
2023-07-20 00:49:21 -0300

    feature:test-nav-anim

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,25 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'phone': '640px',
+			// => @media (min-width: 640px) { ... }
+
+			'tablet': '768px',
+			// => @media (min-width: 768px) { ... }
+
+			'laptop': '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			'desktop': '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('tailwindcss-animated')],
 	background: {
 		'border-radius': '0.8rem',
 		'border': '1px solid #1E2D3D',
