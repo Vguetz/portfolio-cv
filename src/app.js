35664f3f2e9bce835e60a6cce0a86d42bf65bef8
Vguetz <gomezlucas712@gmail.com>
2023-07-26 13:06:14 -0300

    feature:contact-submit

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById('sidebar');
     const mainSection = document.querySelector('main');
-    const mainSectionBottom = mainSection.offsetTop + mainSection.offsetHeight;
+    const aboutMeSection = document.getElementById('about');
 
     // Function to check if the sidebar should be hidden
     const handleScroll = () => {
-        if (window.scrollY > mainSectionBottom) {
+        if (window.scrollY > mainSection.offsetTop + mainSection.offsetHeight) {
             sidebar.classList.add('translate-x-[-100%]');
             sidebar.classList.add('transition-transform');
         } else {
@@ -16,4 +16,21 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Add the scroll event listener
     window.addEventListener('scroll', handleScroll);
+
+    // Function to check if the bottom of the sidebar touches the top of the "About Me" section
+    const handleAboutMeSectionIntersection = (entries) => {
+        const [entry] = entries;
+        if (entry.isIntersecting && entry.boundingClientRect.bottom >= 0) {
+            sidebar.classList.add('translate-x-[-100%]');
+        }
+    };
+
+    // Add the Intersection Observer to watch the "About Me" section
+    const aboutMeObserver = new IntersectionObserver(handleAboutMeSectionIntersection, {
+        root: null, // Use the viewport as the root
+        rootMargin: '0px',
+        threshold: 0,
+    });
+
+    aboutMeObserver.observe(aboutMeSection);
 });
